85f506b3b0365d0a80e654fd70e4b2d2fbe26112
Fitrahtur Rahman <fitrahtur@gmail.com>
2026-07-06 22:33:27 +0700

    feat(lrt): add station constants and domain types

---
+++ src/lib/lrt/constants.ts
@@ -0,0 +1,22 @@
+import { ILRTStation } from './types'
+
+export const LRT_STATIONS: readonly ILRTStation[] = [
+  { slug: 'dukuh-atas-bni', name: 'Dukuh Atas BNI' },
+  { slug: 'setiabudi', name: 'Setiabudi' },
+  { slug: 'rasuna-said', name: 'Rasuna Said' },
+  { slug: 'kuningan', name: 'Kuningan' },
+  { slug: 'pancoran-bank-bjb', name: 'Pancoran bank bjb' },
+  { slug: 'cikoko', name: 'Cikoko' },
+  { slug: 'ciliwung', name: 'Ciliwung' },
+  { slug: 'cawang', name: 'Cawang' },
+  { slug: 'halim', name: 'Halim' },
+  { slug: 'jati-bening-baru', name: 'Jati Bening Baru' },
+  { slug: 'cikunir-1', name: 'Cikunir 1' },
+  { slug: 'cikunir-2', name: 'Cikunir 2' },
+  { slug: 'bekasi-barat', name: 'Bekasi Barat' },
+  { slug: 'jati-mulya', name: 'Jati Mulya' },
+  { slug: 'taman-mini', name: 'Taman Mini' },
+  { slug: 'kampung-rambutan', name: 'Kampung Rambutan' },
+  { slug: 'ciracas', name: 'Ciracas' },
+  { slug: 'harjamukti', name: 'Harjamukti' },
+]
